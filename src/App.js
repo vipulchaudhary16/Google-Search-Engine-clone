bad973ebb4c23b1f4fa9bb30db57cf1c0545fb5c
Vipul Chaudhary <77090657+vipulchaudhary16@users.noreply.github.com>
2021-11-08 19:54:13 +0530

    started working on routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import './App.css';
+import { React, useState } from "react";
+
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Routes from "./components/Router";
 
 function App() {
+  const [darkTheme, setDarkTheme] = useState(false);
+
   return (
-    <div className="App">
+    <div className={darkTheme ? "dark" : ""}>
+      <div className="bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen">
+        <Navbar darkTheme = {darkTheme} setDarkTheme={setDarkTheme}/>
+        <Routes/>
+        <Footer/>
+      </div>
     </div>
   );
 }
