82bb475f07c41b3b2134bc39d4ccdb556be081a2
Vipul Chaudhary <77090657+vipulchaudhary16@users.noreply.github.com>
2021-11-12 16:09:15 +0530

    Style changed

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState, useContext } from "react";
 import ResultContext from "../context/ResultContext";
 
+import "../global.css"
+
 const Search = () => {
   const context = useContext(ResultContext);
   const [tempText, setTempText] = useState('')
@@ -15,22 +17,22 @@ const Search = () => {
   }, [text]);
 
   return (
-    <div className="relative sm:ml-48 md:ml-72 sm:-mt-10 mt-3">
+    <div className="flex flex-wrap items-center content-center relative sm:ml-48 sm:-mt-10 mt-3">
       <input
         value={tempText}
         type="text"
-        className="sm:w-96 w-80 h-10 dark:bg-gray-200  border rounded-full shadow-sm outline-none p-6 text-black hover:shadow-lg"
+        className=" w-80 h-10 dark:bg-gray-200  border rounded-full shadow-sm outline-none p-6 text-black hover:shadow-lg"
         placeholder="🔎 Search Google or type URL"
         onChange={(e) => setTempText(e.target.value)}
       />
       <button
-        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded ml-2"
+        class="search-button bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded-full ml-2"
         onClick={(e) => {
           setText(tempText);
             buttinClicked = true;
         }}
       >
-        Button
+        Search
       </button>
       {/* {text !== "" && (
         <button
